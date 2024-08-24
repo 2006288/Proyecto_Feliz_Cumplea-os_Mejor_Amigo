c2a9601431323d26b8922c3768adb88af66d2861
2006288 <anthonyescuela@gmail.com>
2024-08-24 15:39:27 -0500

    Corecciones de tamaño

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ function escribir() {
                     if (charIndex < segment.length) {
                         container.innerHTML += segment.charAt(charIndex);
                         charIndex++;
-                        setTimeout(typeChar, 30); // Ajusta la velocidad aquí (en milisegundos) 40
+                        setTimeout(typeChar, 20); // Ajusta la velocidad aquí (en milisegundos) 40
                     } else {
                         typeSegment(segmentIndex + 1);
                     }
